8f4598cc81a1a97f7befde3ef270f3fc42502bdc
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-07-30 23:32:40 -0300

    Validation

--- src/tareas/dto/tareas.dto.ts
+++ src/tareas/dto/tareas.dto.ts
@@ -1,10 +1,22 @@
 import { EstadoTareas } from "../tareas.entity"
+import { IsString, MinLength, IsNotEmpty, IsOptional, IsIn } from "class-validator"
 export class creatTarea {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   titulo: string;
+  @IsString()
   descripcion: string;
 }
 export class updaterTarea {
+  @IsString()
+  @IsOptional()
   titulo?: string;
+  @IsString()
+  @IsOptional()
   descripcion?: string;
+  @IsString()
+  @IsOptional()
+  @IsIn([EstadoTareas.DONE, EstadoTareas.IN_PROGRESS, EstadoTareas.PENDING])
   status?: EstadoTareas;
 }
